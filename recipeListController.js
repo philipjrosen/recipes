1eebc4d017b7fb75074cabbd35149f80755bca9f
Philip Rosen <philipjrosen@gmail.com>
2015-08-15 13:55:18 -0700

    adds checkboxes, object to store ingredient lists and functions to update storage object

--- recipeListController.js
+++ recipeListController.js
@@ -2,6 +2,23 @@ angular.module('app.recipeList', [])
 
 .controller('recipeListCtrl', ['$scope', "recipeFactory", function ($scope, recipeFactory) {
 
+  $scope.ingredientLists = {};
+
+  var addToIngredientsList = function(index, name) {
+    $scope.ingredientLists[name] = $scope.recipes[index].ingredients;
+  };
+
+  var removeFromIngredientsList = function(name) {
+    delete $scope.ingredientLists[name];
+  };
+
+  $scope.updateIngredientLists = function(include,index) {
+    var name = $scope.recipes[index].name;
+    include ?
+      addToIngredientsList(index, name) :
+      removeFromIngredientsList(name);
+  };
+
   $scope.getRecipes = function() {
     recipeFactory.getRecipes()
     .then(function (recipes) {
