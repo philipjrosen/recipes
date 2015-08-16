88dc2a05d0ce208c12ea7303a26de540b9237c9c
Philip Rosen <philipjrosen@gmail.com>
2015-08-15 19:25:14 -0700

    remove unused functions from recipeListCtrl

--- recipeListController.js
+++ recipeListController.js
@@ -2,9 +2,6 @@ angular.module('app.recipeList', [])
 
 .controller('recipeListCtrl', ['$scope', "recipeFactory", function ($scope, recipeFactory) {
 
-  var ingredientLists = {};
-  $scope.distinctList = [];
-
   var addCheckedProperty = function(arr) {
     _.forEach(arr, function(obj) {
       obj.checked = false;
@@ -15,34 +12,6 @@ angular.module('app.recipeList', [])
     checked ?
       $scope.recipes[index].checked = true :
       $scope.recipes[index].checked = false;
-  };
-
-  var addToIngredientsList = function(index, name) {
-    ingredientLists[name] = $scope.recipes[index].ingredients;
-  };
-
-  var removeFromIngredientsList = function(name) {
-    delete ingredientLists[name];
-  };
-
-  var makeDistinctList = function(obj) {
-    var selected = [];
-
-    _.forEach(obj, function(elem){
-      selected.push(elem);
-    });
-
-    return _.union.apply(null, selected);
-  };
-
-  $scope.updateIngredientLists = function(include,index) {
-    var name = $scope.recipes[index].name;
-
-    include ?
-      addToIngredientsList(index, name) :
-      removeFromIngredientsList(name);
-
-    $scope.distinctList = makeDistinctList(ingredientLists);
   };
 
   $scope.getRecipes = function() {
