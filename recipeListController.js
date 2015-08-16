aabf7c17ba8df27efe3138edafad5190eee8947b
Philip Rosen <philipjrosen@gmail.com>
2015-08-16 11:32:18 -0700

    add persistence for ingredient to filter by

--- recipeListController.js
+++ recipeListController.js
@@ -19,11 +19,17 @@ angular.module('app.recipeList', [])
   var init = function() {
     if (localStorageService.isSupported) {
       var recipesInStore = localStorageService.get('recipes');
+      var ingredientInStore = localStorageService.get('ingredient');
 
       $scope.recipes = recipesInStore || $scope.getRecipes();
+      $scope.ingredient = ingredientInStore || "";
 
       $scope.$watch('recipes', function () {
         localStorageService.set('recipes', $scope.recipes);
+      }, true);
+
+      $scope.$watch('ingredient', function () {
+        localStorageService.set('ingredient', $scope.ingredient);
       }, true);
     }
   };
